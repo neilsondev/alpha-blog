3f65ba168efa61c889d9584bfee3f9cd760cb8f5
neilsondev <jose.neilson@virtual.ufc.br>
2025-09-02 11:24:09 -0300

    add user show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def gravatar_for(user, options = { size: 80 })
+    email_address = user.email.downcase
+    hash = Digest::SHA256.hexdigest(email_address)
+
+    size = options[:size]
+    params = URI.encode_www_form('s' => size)
+    image_src = "https://www.gravatar.com/avatar/#{hash}?#{params}"
+
+    image_tag(image_src, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
 end
