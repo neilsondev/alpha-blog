389adfd0f300a740f39a77269248509846e15f55
neilsondev <jose.neilson@virtual.ufc.br>
2025-09-15 15:30:14 -0300

    create many-to-many association between articles and categories

---
+++ db/migrate/20250915181642_create_article_categories.rb
@@ -0,0 +1,9 @@
+class CreateArticleCategories < ActiveRecord::Migration[8.0]
+  def change
+    create_table :article_categories do |t|
+      t.belongs_to :article
+      t.belongs_to :category
+      t.timestamps
+    end
+  end
+end
