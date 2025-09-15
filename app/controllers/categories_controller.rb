ae9effe55293b2d21395a67eb999c5c24287c59e
neilsondev <jose.neilson@virtual.ufc.br>
2025-09-15 13:42:32 -0300

    add create category test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,20 @@ class CategoriesController < ApplicationController
 
   def new
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created."
+      redirect_to category_url(@category)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
